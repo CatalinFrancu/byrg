d41176a0eb7527731077f39008c22293fddd9d2c
Cătălin Frâncu <cata@francu.com>
2024-06-20 09:49:09 +0300

    Implement BFS, but don't actually use it.

--- src/Board.cpp
+++ src/Board.cpp
@@ -22,8 +22,97 @@ void Board::initPlayerMasks() {
   inHand[0] = inHand[1] = (1 << NUM_PIECES) - 1;
 }
 
-int Board::eval() {
-  return 0;
+int Board::eval(int player) {
+  int result = SCORE_SELF * playerEval(player)
+    - SCORE_OPP * playerEval(1 - player);
+  // int result = SCORE_OPP * sumStoneDist(1 - player) -
+  //   SCORE_SELF * sumStoneDist(player);
+  return result;
+}
+
+int Board::playerEval(int player) {
+  // count stones and cells
+  int numStones = 0;
+  int numCells = 0;
+  for (u8 r = 1; r <= BOARD_SIZE; r++) {
+    for (u8 c = 1; c <= BOARD_SIZE; c++) {
+      if (isAvailable(player, r, c) &&
+          ((a[r - 1][c - 1] == player) ||
+           (a[r - 1][c + 1] == player) ||
+           (a[r + 1][c - 1] == player) ||
+           (a[r + 1][c + 1] == player))) {
+        numStones++;
+      }
+      if (a[r][c] == player) {
+        numCells++;
+      }
+    }
+  }
+
+  if (!numStones) {
+    numStones = 1;
+  }
+
+  int score = numStones + numCells * SCORE_LARGE_PIECES;
+  return score;
+}
+
+int Board::sumStoneDist(int player) {
+  bfsInit(player);
+  bfs(player);
+  return bfsCollect(player);
+}
+
+void Board::bfsInit(int player) {
+  for (int r = 0; r < PADDED_BOARD_SIZE; r++) {
+    for (int c = 0; c < PADDED_BOARD_SIZE; c++) {
+      dist[r][c] = MAX_DIST;
+    }
+  }
+  head = 0;
+  tail = collectStones(player, queue);
+  for (int i = 0; i < tail; i++) {
+    dist[queue[i].r][queue[i].c] = 0;
+  }
+}
+
+void Board::bfs(int player) {
+  while (head != tail) {
+    Cell cell = queue[head++];
+    int d = dist[cell.r][cell.c];
+    bfsExtend(player, cell.r - 1, cell.c, d + 1);
+    bfsExtend(player, cell.r, cell.c - 1, d + 1);
+    bfsExtend(player, cell.r + 1, cell.c, d + 1);
+    bfsExtend(player, cell.r, cell.c + 1, d + 1);
+  }
+}
+
+void Board::bfsExtend(int player, u8 r, u8 c, int d) {
+  if ((dist[r][c] == MAX_DIST) &&
+      isAvailable(player, r, c)) {
+    dist[r][c] = d;
+    queue[tail++] = { r, c };
+  }
+}
+
+int Board::bfsCollect(int player) {
+  int result = 0;
+
+  for (int r = 1; r <= BOARD_SIZE; r++) {
+    for (int c = 1; c <= BOARD_SIZE; c++) {
+      if (isAvailable(player, r, c)) {
+        if (dist[r][c] == MAX_DIST) {
+          result += MAX_DIST;
+        } else {
+          result += (int)dist[r][c] * dist[r][c];
+        }
+      } else if (a[r][c] == EMPTY) {
+        result += SCORE_DEAD;
+      }
+    }
+  }
+
+  return result;
 }
 
 void Board::setArea(int val, Move& move) {
@@ -49,11 +138,7 @@ int Board::collectStones(int player, Cell* dest) {
   int n = 0;
   for (u8 r = 1; r <= BOARD_SIZE; r++) {
     for (u8 c = 1; c <= BOARD_SIZE; c++) {
-      if ((a[r][c] == EMPTY) &&
-          (a[r - 1][c] != player) &&
-          (a[r][c - 1] != player) &&
-          (a[r + 1][c] != player) &&
-          (a[r][c + 1] != player) &&
+      if (isAvailable(player, r, c) &&
           ((a[r - 1][c - 1] == player) ||
            (a[r - 1][c + 1] == player) ||
            (a[r + 1][c - 1] == player) ||
@@ -73,38 +158,45 @@ bool Board::accommodates(PieceVariant var, int player) {
   for (int i = 0; i < var.size; i++) {
     int rank = var.cells[i] / PADDED_BOARD_SIZE;
     int file = var.cells[i] % PADDED_BOARD_SIZE;
-    if ((a[rank][file] != EMPTY) ||
-        (a[rank][file - 1] == player) ||
-        (a[rank - 1][file] == player) ||
-        (a[rank][file + 1] == player) ||
-        (a[rank + 1][file] == player)) {
+    if (!isAvailable(player, rank, file)) {
       return false;
     }
   }
   return true;
 }
 
+bool Board::isAvailable(int player, int rank, int file) {
+  return
+    (rank >= 0) && (rank < PADDED_BOARD_SIZE) &&
+    (file >= 0) && (file < PADDED_BOARD_SIZE) &&
+    (a[rank][file] == EMPTY) &&
+    (a[rank][file - 1] != player) &&
+    (a[rank - 1][file] != player) &&
+    (a[rank][file + 1] != player) &&
+    (a[rank + 1][file] != player);
+}
+
 void Board::print() {
   for (int rank = BOARD_SIZE; rank >= 1; rank--) {
-    printf("%2d ", rank);
+    fprintf(stderr, "%2d ", rank);
     for (int file = 1; file <= BOARD_SIZE; file++) {
       printCell(rank, file);
     }
-    printf("\n");
+    fprintf(stderr, "\n");
   }
 
-  printf("   ");
+  fprintf(stderr, "   ");
   for (int file = 0; file < BOARD_SIZE; file++) {
-    printf(" %c", file + 'a');
+    fprintf(stderr, " %c", file + 'a');
   }
-  printf("\n");
+  fprintf(stderr, "\n");
 }
 
 void Board::printCell(int rank, int file) {
   if (a[rank][file] == EMPTY) {
-    printf("  ");
+    fprintf(stderr, "  ");
   } else {
     int p = a[rank][file];
-    printf("%s  %s", ANSI_COLORS[p], DEFAULT_COLOR);
+    fprintf(stderr, "%s  %s", ANSI_COLORS[p], DEFAULT_COLOR);
   }
 }
--- src/Board.h
+++ src/Board.h
@@ -8,14 +8,18 @@
 class Board {
 public:
   static const u8 EMPTY = 2;
+  static const u8 MAX_DIST = 255;
   static constexpr Cell STARTING_POSITIONS[2] = { { 10, 5 }, { 5, 10 } };
-  static const int COEF_POP = 4;
-  static const int COEF_STONES = 1;
 
   static constexpr char ANSI_COLORS[2][7] = {
     "\e[105m", "\e[43m",
   };
   static constexpr char DEFAULT_COLOR[7] = "\e[49m";
+
+  static const int SCORE_DEAD = 200;
+  static const int SCORE_SELF = 1;
+  static const int SCORE_OPP = 1;
+  static const int SCORE_LARGE_PIECES = 2;
 
   // 0-1 = used by player 0-1; EMPTY = empty
   u8 a[PADDED_BOARD_SIZE][PADDED_BOARD_SIZE];
@@ -25,15 +29,28 @@ public:
   // bit masks of pieces still in hand
   int inHand[2];
 
+  // used during the eval BFS
+  // TODO: These are only used for Minimax leaves
+  u8 dist[PADDED_BOARD_SIZE][PADDED_BOARD_SIZE];
+  Cell queue[PADDED_BOARD_SIZE * PADDED_BOARD_SIZE];
+  int head, tail;
+
   void init();
 
-  int eval();
+  int eval(int player);
+  int playerEval(int player);
+  int sumStoneDist(int player);
+  void bfsInit(int player);
+  void bfs(int player);
+  void bfsExtend(int player, u8 r, u8 c, int d);
+  int bfsCollect(int player);
 
   void setArea(int val, Move& move);
   void makeMove(int player, Move& move);
   void undoMove(int player, Move& move);
   int collectStones(int player, Cell* dest);
   bool accommodates(PieceVariant var, int player);
+  bool isAvailable(int player, int rank, int file);
   void print();
   void printCell(int rank, int file);
 
